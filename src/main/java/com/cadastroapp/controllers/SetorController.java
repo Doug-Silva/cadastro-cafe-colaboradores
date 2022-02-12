27565a331716c5ad31b6a78861f584cf4ab8b397
Douglas Silva <silvatec.br@gmail.com>
2022-02-12 20:21:31 -0300

    Criando form cadastro de colaboradores e refeicao

--- src/main/java/com/cadastroapp/controllers/SetorController.java
+++ src/main/java/com/cadastroapp/controllers/SetorController.java
@@ -2,6 +2,7 @@ package com.cadastroapp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,24 +16,33 @@ public class SetorController {
 	@Autowired
 	private SetorRepository sr;
 	
-	@RequestMapping(value="/cadastrarSetor", method=RequestMethod.GET)
+	@RequestMapping(value="/cadastrar-setor", method=RequestMethod.GET)
 	public String form() {
-		return "cadastroSetor/formCadastroSetor";
+		return "cadastroColaboradores/formCadastroSetor";
 	}
 	
-	@RequestMapping(value="/cadastrarSetor", method=RequestMethod.POST)
+	@RequestMapping(value="/cadastrar-setor", method=RequestMethod.POST)
 	public String form(Setor setor) {
 		
 		sr.save(setor);
 		
-		return "redirect:/cadastrarSetor";
+		return "redirect:/cadastrar-setor";
 	}
 	
-	@RequestMapping("/setores")
+	@RequestMapping("/lista-cadastros")
 	public ModelAndView listaSetores(){
 		ModelAndView mv = new ModelAndView("index");
 		Iterable<Setor> setores = sr.findAll();
 		mv.addObject("setores", setores);
+		return mv;
+	}
+	
+
+	@RequestMapping("/{codigo}")
+	public ModelAndView detalhesSetor(@PathVariable("codigo") long codigo){
+		Setor setor = sr.findByCodigo(codigo);
+		ModelAndView mv = new ModelAndView("cadastroColaboradores/formCadastroColaborador");
+		mv.addObject("setor", setor);
 		return mv;
 	}
 	
--- src/main/java/com/cadastroapp/repository/SetorRepository.java
+++ src/main/java/com/cadastroapp/repository/SetorRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import com.cadastroapp.models.Setor;
 
 public interface SetorRepository extends CrudRepository<Setor, String>{
+	Setor findByCodigo(long codigo);
 
 }
