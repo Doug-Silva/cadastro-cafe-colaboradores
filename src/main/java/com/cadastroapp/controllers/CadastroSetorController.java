66cfba6adf8440c2f1fba9723b276c21012cf6db
Douglas Silva <silvatec.br@gmail.com>
2022-02-12 04:23:32 -0300

    Criando tabela cadastro de setor dos colaboradores e data config do banco de dados

--- src/main/java/com/cadastroapp/controllers/CadastroSetorController.java
+++ src/main/java/com/cadastroapp/controllers/CadastroSetorController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CadastroSetorController {
 	
-	@RequestMapping("cadastrarSetor")
+	@RequestMapping("/cadastrarSetor")
 	public String form() {
 		return "cadastroSetor/formCadastroSetor";
 	}
--- src/main/java/com/cadastroapp/models/CadastroSetor.java
+++ src/main/java/com/cadastroapp/models/CadastroSetor.java
@@ -1,13 +1,32 @@
 package com.cadastroapp.models;
 
-//CADASTRAR CAFÉ DA MANHÃ DOS COLABORADORES, SEPARADO PELO SETOR
-public class CadastroSetor {
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class CadastroSetor implements Serializable{
 	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long codigo;
+	
+	//CADASTRAR CAFÉ DA MANHÃ DOS COLABORADORES, SEPARADO PELO SETOR
 	private String nomeSetor;
 	private String nomeSupervisor;
 	private String dataCadastro;
 	private String horaCadastro;
 	
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
 	public String getNomeSetor() {
 		return nomeSetor;
 	}
