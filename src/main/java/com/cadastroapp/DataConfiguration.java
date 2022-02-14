be2a4e90ea5405a06e49b4d6b1836179ff5b1fe8
Douglas Silva <silvatec.br@gmail.com>
2022-02-14 19:20:15 -0300

    config mysql

--- src/main/java/com/cadastroapp/DataConfiguration.java
+++ src/main/java/com/cadastroapp/DataConfiguration.java
@@ -17,19 +17,24 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 public class DataConfiguration {
 	
 	@Bean
-    public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+	public DataSource dataSource(){
+	    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+	    dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+	    dataSource.setUrl("jdbc:mysql://localhost:3306/cadastroapp");
+	    dataSource.setUsername("root");
+	    dataSource.setPassword("douglas442");
+	    return dataSource;
+	}
 
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
-
-        return basicDataSource;
-    }
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter(){
+	    HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+	    adapter.setDatabase(Database.MYSQL);
+	    adapter.setShowSql(true);
+	    adapter.setGenerateDdl(true);
+	    adapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+	    adapter.setPrepareConnection(true);
+	    return adapter;
+	}
 	
 }
