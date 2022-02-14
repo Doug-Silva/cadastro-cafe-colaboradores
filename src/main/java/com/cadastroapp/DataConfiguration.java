98af902f1a240c0fb5346765675a8a61d90a0c2c
Douglas Silva <silvatec.br@gmail.com>
2022-02-14 20:04:29 -0300

    config mysql 3.0

--- src/main/java/com/cadastroapp/DataConfiguration.java
+++ src/main/java/com/cadastroapp/DataConfiguration.java
@@ -1,11 +1,6 @@
 package com.cadastroapp;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
 import javax.sql.DataSource;
-
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -16,25 +11,26 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 @Configuration
 public class DataConfiguration {
 	
+	//CONEXÃO HEROKU MYSQL
 	@Bean
 	public DataSource dataSource(){
-	    DriverManagerDataSource dataSource = new DriverManagerDataSource();
-	    dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-	    dataSource.setUrl("jdbc:mysql://pfw0ltdr46khxib3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/itn4ip5h3k2ax127");
-	    dataSource.setUsername("qjsy5wkoyo6gox0q");
-	    dataSource.setPassword("y1pck6uqynquz5iz");
-	    return dataSource;
-	}
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://pfw0ltdr46khxib3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/itn4ip5h3k2ax127");
+		dataSource.setUsername("qjsy5wkoyo6gox0q");
+		dataSource.setPassword("y1pck6uqynquz5iz");
+		return dataSource;
 
-	@Bean
-	public JpaVendorAdapter jpaVendorAdapter(){
-	    HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-	    adapter.setDatabase(Database.MYSQL);
-	    adapter.setShowSql(true);
-	    adapter.setGenerateDdl(true);
-	    adapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
-	    adapter.setPrepareConnection(true);
-	    return adapter;
 	}
 	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter(){
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+	}
 }
