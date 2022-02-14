1b252497b20419bbdeb8a01eac8c1cc50f197ecc
Douglas Silva <silvatec.br@gmail.com>
2022-02-14 19:31:18 -0300

    config mysql 2.0

--- src/main/java/com/cadastroapp/DataConfiguration.java
+++ src/main/java/com/cadastroapp/DataConfiguration.java
@@ -20,9 +20,9 @@ public class DataConfiguration {
 	public DataSource dataSource(){
 	    DriverManagerDataSource dataSource = new DriverManagerDataSource();
 	    dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-	    dataSource.setUrl("jdbc:mysql://localhost:3306/cadastroapp");
-	    dataSource.setUsername("root");
-	    dataSource.setPassword("douglas442");
+	    dataSource.setUrl("jdbc:mysql://pfw0ltdr46khxib3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/itn4ip5h3k2ax127");
+	    dataSource.setUsername("qjsy5wkoyo6gox0q");
+	    dataSource.setPassword("y1pck6uqynquz5iz");
 	    return dataSource;
 	}
 
